172ead306fcf2f46453e8ec17307733b81c09551
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-07-26 09:09:59 +0530

    String

--- ws_15_strings.java
+++ ws_15_strings.java
@@ -10,12 +10,17 @@ public class ws_15_strings {
      * Since string is a class , it can have object : String s = new String(); -> it will create an immutable object
      * String s = "yash" , it is also consider as an object 
      * To create String , there are three classes : 1)String     2)StringBuffer      3)StringBuilder 
+     * String contant pool is in heap area after 1.7 java version , before it was method area
+     * String are immutable in java because string objects are cached in string pool. since cached string literals are shared between multiple persons there is always a risk, where one person's action would affect all all another persons , for example is one person changes its city from "yash" to "anubhav" all other persons will also get affected.
      */
     public static void main(String[] args) {
         char [] c = {'a', 'n','u','b','h','a','v'};
-        String s = new String(c);
-        System.out.println(c);
-        System.out.println(s);        
+        String s3 = new String(c);
+        String s = new String("yash");   //string object created using new keyword allocate in heap area pointing to s and also another object created in string contant pool pointing to reference variable created by JVM 4 future use  , hence using new keyword there is max 2 object created and there is no garbage collection 
+        String s1 = "Yash"; // in this case string literal create a memory in string contant pool only for future use 
+        System.out.println(s3);
+        System.out.println(s);
+        System.out.println(s1);        
     }
 
 }
