26a68f64254cf44636feefcf6d4af34d80b18740
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-09-04 01:17:49 +0530

    ExceptionHandling

---
+++ ExceptionHandling/EH_01.java
@@ -0,0 +1,8 @@
+/*
+ * Difference between Error and Exception
+ * 1. Error cant be handled whereas Exception can be handled.
+ * Exceptions and errors both are subclasses of Throwable class. The error indicates a problem that mainly occurs due to the lack of system resources and our application should not catch these types of problems. Some of the examples of errors are system crash error and out of memory error. Errors mostly occur at runtime that's they belong to an unchecked type. 
+*
+* Exceptions are the problems which can occur at runtime and compile time. It mainly occurs in the code written by the developers.  Exceptions are divided into two categories such as checked exceptions and unchecked exceptions. 
+
+ */
