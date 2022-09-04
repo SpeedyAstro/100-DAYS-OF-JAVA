dd3a82f93282d715c0b50b75adb486ec215e7496
Anubhav Pandey <pandeyanubhav888@gmail.com>
2022-09-04 23:54:56 +0530

    Collections

---
+++ Collections/collection.java
@@ -0,0 +1,27 @@
+package Collections;
+
+public class collection {
+    /*
+     * collections is a single entitity which can store multiple objects / data 
+     * collections framework can store only non-primitive data type (objects)
+     * 
+     * Framework - represents libraries 
+     * 
+     * collection - > 
+     *  collection is an interface which is present in java.util package
+     * Syntax - public interface Collection<E> extands Iterable<E> { - }
+     * -> Collection is an object which is used to represnt a group of individual objects as a single unit
+     * -> Collection is the root interface of Collection Framework
+     * -> There is no concrete class which implements the collection interface directly but there are interface which 
+     * 
+     * -> Collection interface contains most of the common method which are applicable for any collection object
+     *  -> most of collection object
+     * 1.boolean add(Object a);
+     * 2.boolean addAll(Collection c);
+     * 3.boolean remove(Object obj);
+     * 4.boolean removeAll(Collection c);
+     * 5.default boolean removeIf(-) { - }
+     * 6.boolean retainAll(Collection c);  retain the passed agrument and clear everything in a collection
+     * 7.void clear();
+     */
+}
