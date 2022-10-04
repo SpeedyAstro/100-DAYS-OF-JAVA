94c0a80266cd9394e00ae6c3021257ba019ed778
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-10-04 22:53:44 +0530

    Linkedlist

---
+++ Collections/ImplementLinkedlist.java
@@ -0,0 +1,19 @@
+package Collections;
+
+class SinglyLinkedList{
+    private LinkNode head;
+    private static class LinkNode{
+        private int data;
+        private LinkNode next;
+        LinkNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
+
+public class ImplementLinkedlist {
+    public static void main(String[] args) {
+
+    }
+}
