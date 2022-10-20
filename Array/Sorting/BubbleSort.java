1d58cb13b795a5154bec6481d0961f02167c9dc4
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-10-20 19:24:34 +0530

    BubbleSort

--- Array/Sorting/BubbleSort.java
+++ Array/Sorting/BubbleSort.java
@@ -3,14 +3,20 @@ public class BubbleSort {
         
     }
     public static void bubble(int []arr ){
+        boolean flag ;
         for(int i = 0 ; i<arr.length ; i++){
             for(int j = 1; j < arr.length-i;j++){
                 if(arr[j]<arr[j-1]){
+                    flag = true;
                     // swap 
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                 }
+            }
+            // if no element swap through the pass then list must be sorted 
+            if(!flag){
+                break;
             }
         }
     }
