d3c045df15e6e26c885676a76eec7778c356e69e
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-12-07 19:18:15 +0530

    SortedLL

--- Collections/SortedLinkedList.java
+++ Collections/SortedLinkedList.java
@@ -27,4 +27,29 @@ public class SortedLinkedList {
         newNode.next = current;
         temp.next = newNode;
     }
+    //remove node
+    public void deleteNode(int value){
+        LinkNode newNode = new LinkNode(value);
+        if(head==null) System.out.println("no linked list found!");
+        LinkNode current = head;
+        LinkNode temp = null;
+        if(current!=null&&current.data==value) { head = current.next; return;}
+        while (current!=null&&current.data!=newNode.data){
+            temp = current;
+            current = current.next;
+        }
+        if(current==null) return;
+        temp.next = current.next;
+    }
+    // check to detect a loop in Singly Linked list
+    public static boolean detectLoop(){
+        LinkNode fastPtr = head;
+        LinkNode slowptr = head;
+        while (fastPtr!=null && fastPtr.next!=null){
+            fastPtr = fastPtr.next.next;
+            slowptr = slowptr.next;
+            if(fastPtr==slowptr) return true;
+        }
+        return false;
+    }
 }
