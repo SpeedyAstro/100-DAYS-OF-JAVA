0529539edbfb04d701fff6106b04bd7a4cb47ac5
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-12-30 00:35:41 +0530

    DoublyLL

---
+++ Collections/doublyLL/DoublyLL.java
@@ -0,0 +1,9 @@
+package Collections.doublyLL;
+// it is called two-way linked list.
+// Given a node, we can navigate list in both forward and backward direction, which is not possible in singly linked list.
+// a node in singly linked list can only be deleted if we have a pointer to its previous node, but in doubly linked list we can delete the node even if we don't have pointer to its previous node.
+//  <--[previous | data | next ]-->
+
+public class DoublyLL {
+
+}
