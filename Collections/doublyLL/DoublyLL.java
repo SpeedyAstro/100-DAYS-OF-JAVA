3dfa81d6be3d8ac7e0bef6d7181b99fcc0e041c4
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-12-30 23:15:10 +0530

    `DLL`

--- Collections/doublyLL/DoublyLL.java
+++ Collections/doublyLL/DoublyLL.java
@@ -6,4 +6,27 @@ package Collections.doublyLL;
 
 public class DoublyLL {
 
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+    private class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+    public DoublyLL(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    public boolean isEmpty(){
+        return length==0;
+    }
+    public int getLength(){
+        return length;
+    }
 }
