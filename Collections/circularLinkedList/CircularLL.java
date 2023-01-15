4daef8c6217d37853a89fa451f9e10b191bf3e88
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2023-01-15 23:46:05 +0530

    CircularLinkedList

--- Collections/circularLinkedList/CircularLL.java
+++ Collections/circularLinkedList/CircularLL.java
@@ -1,2 +1,40 @@
-package Collections.circularLinkedList;public class CircularLL {
+package Collections.circularLinkedList;
+
+public class CircularLL {
+    private ListNode last;
+    private int length;
+
+    private class ListNode{
+        private ListNode next;
+        private int data;
+        public ListNode(int data){
+            this.data = data;
+        }
+    }
+    public CircularLL(){
+        last = null;
+        length = 0;
+    }
+    public int getLength(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length==0;
+    }
+    public void createCircularLL(){
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(1);
+        ListNode fourth = new ListNode(1);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+        last = fourth;
+    }
+
+    public static void main(String[] args) {
+        CircularLL circularLL = new CircularLL();
+        circularLL.createCircularLL();
+    }
 }
