410d46ae7f70b51d06ab385cd5c1ab02b7884231
Anubhav Pandey <57632430+SpeedyAstro@users.noreply.github.com>
2024-01-06 10:28:06 +0530

    Binary Search

--- Array/BinarySearch/RotationCount.java
+++ Array/BinarySearch/RotationCount.java
@@ -21,4 +21,58 @@ public class RotationCount {
             }
             return -1;
     }
+    static int pivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1; // array is not rotated
+    }
+    static int findPivotWithDuplicates(int[] arr){
+        int start = 0 ;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + ((end - start) / 2);
+            if(mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid - 1]){
+                return mid - 1;
+            }
+            if(arr[mid] == arr[start]  && arr[mid] == arr[end]){
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot?
+                // check if start is pivot
+                if(start<end && arr[start] > arr[start + 1]){
+                    return start;
+                }
+                start++;
+                // check whether end is pivot
+                if(arr[end] < arr[end - 1]){
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
